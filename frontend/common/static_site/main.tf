86816eddc2889c0b40bbd8a3199daac1d574506d
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-11-12 20:12:25 +0900

    fix: s3 arn 수정

--- frontend/common/static_site/main.tf
+++ frontend/common/static_site/main.tf
@@ -33,21 +33,16 @@ resource "aws_s3_bucket_policy" "s3_policy" {
 # s3 버킷 정책 내용 data
 data "aws_iam_policy_document" "s3_access_policy" {
     statement {
-      sid = "1"
       effect = "Allow"
       principals {
         type = "AWS"
         identifiers = ["*"]
       }
       actions = [
-        "s3:PutObject",
-        "s3:PutObjectAcl",
         "s3:GetObject",
-        "s3:GetObjectAcl",
-        "s3:DeleteObject"
       ]
       resources = [
-        aws_s3_bucket.s3.arn
+        "${aws_s3_bucket.example.arn}/*"
       ]
     }
 }
